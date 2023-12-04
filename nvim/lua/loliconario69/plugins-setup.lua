b78b4804df2a994f7a6036b8db86119768de9579
Diego Ibañez <loliconario69@gmail.com>
2023-12-03 21:49:02 -0300

    Added comentaries for plugins

--- nvim/lua/loliconario69/plugins-setup.lua
+++ nvim/lua/loliconario69/plugins-setup.lua
@@ -12,34 +12,43 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
+  -- essentials
   use 'wbthomason/packer.nvim'
-  use 'nvim-lua/plenary.nvim'
+  use 'nvim-lua/plenary.nvim'  -- required by others plugins
 
+  -- color scheme
   use 'bluz71/vim-nightfly-guicolors'
+
+  -- tmux comatibility and pane focus for nvim
   use 'christoomey/vim-tmux-navigator'
   use 'szw/vim-maximizer'
-  use 'tpope/vim-surround' -- add, delete, change surroundings
+ 
+  -- nvim-tree and icons
+  use 'nvim-tree/nvim-tree.lua'
+  use 'nvim-tree/nvim-web-devicons'
+
+  -- lualine for bottom status line
+  use("nvim-lualine/lualine.nvim")
   
+  -- fuzzy finding w/ telescope (search)
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder, require BurntSushi/ripgrep for live_grep 
+
+  -- syntax highlight
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- utilities
+  use 'tpope/vim-surround' -- add, delete, change surroundings
   use {
     'numToStr/Comment.nvim',
     config = function()
         require('Comment').setup()
     end
   }
-  use 'nvim-tree/nvim-tree.lua'
-
-  use 'nvim-tree/nvim-web-devicons'
-  use("nvim-lualine/lualine.nvim")
-  
-  -- fuzzy finding w/ telescope
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
-
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-
+ 
   -- git integration
   use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
